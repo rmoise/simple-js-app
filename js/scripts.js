25fb2d3658a504b1dacbfca2bcda7bcb9554013b
rmoise <roderickfmoise@gmail.com>
2022-09-05 22:28:35 +0200

    refactor: Remove all existing code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert("Hello world");
-let favoriteFood = "Cheeseburger";
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Pikachu', height: 0.4, type: 'electric' },
+    { name: 'Squirtle', height: 0.5, type: 'water' },
+    { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
+]
