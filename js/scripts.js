e11a923ce8c8783634f4bec44dfeff85700aea6a
rmoise <roderickfmoise@gmail.com>
2022-09-06 08:54:57 +0200

    feat: :sparkles: add pokemon list with three objects to the array

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    { name: 'Pikachu', height: 0.4, type: 'electric' },
+    { name: 'Squirtle', height: 0.5, type: 'water' },
+    { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
+]
