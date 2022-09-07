76611e9658b693e5d402941df59f8a4f9bddf3bb
rmoise <roderickfmoise@gmail.com>
2022-09-07 14:42:50 +0200

    feat: :sparkles: Add conditional that highlights special pokemon in list

--- js/scripts.js
+++ js/scripts.js
@@ -21,12 +21,20 @@ the condition (i <= 100)
 the action (i++)
 */
 for (let i = 0; i < pokemonList.length; i++) {
-    /* pokemonList[i].name:
+    if (pokemonList[i].height > 1.0) {
+        document.write(
+            pokemonList[i].name +
+                ` (height: ${pokemonList[i].height})` +
+                " - Wow, that's big!"
+        )
+    } else {
+        /* pokemonList[i].name:
     meaning give me the pokemon list name at position i, position i being 0, then 1, then 2 until the last object in the array
     Template literal form aka Template Strings:
     This is what a template literal form looks like: `hi ${message}` allows you to write anything between the backtick symbols.
     */
-    document.write(
-        pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '<br>'
-    )
+        document.write(
+            pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '<br>'
+        )
+    }
 }
