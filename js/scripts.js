0caebdcc565591d7121f71d0465f51578578dcd9
rmoise <roderickfmoise@gmail.com>
2022-09-07 16:55:40 +0200

    style: :art: clear some whitespace on document.write

--- js/scripts.js
+++ js/scripts.js
@@ -23,18 +23,14 @@ the action (i++)
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.0) {
         document.write(
-            pokemonList[i].name +
-                ` (height: ${pokemonList[i].height})` +
-                " - Wow, that's big!"
+            pokemonList[i].name + ` (height: ${pokemonList[i].height})` + " - Wow, that's big!"
         )
     } else {
         /* pokemonList[i].name:
-    meaning give me the pokemon list name at position i, position i being 0, then 1, then 2 until the last object in the array
-    Template literal form aka Template Strings:
-    This is what a template literal form looks like: `hi ${message}` allows you to write anything between the backtick symbols.
-    */
-        document.write(
-            pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '<br>'
-        )
+        meaning give me the pokemon list name at position i, position i being 0, then 1, then 2 until the last object in the array
+        Template literal form aka Template Strings:
+        This is what a template literal form looks like: `hi ${message}` allows you to write anything between the backtick symbols.
+        */
+        document.write(pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '<br>')
     }
 }
