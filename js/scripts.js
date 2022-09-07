a491bb3810d714cfa5b28ee212698c0e42c309b1
rmoise <roderickfmoise@gmail.com>
2022-09-07 13:08:11 +0200

    feat: :sparkles: Add a for loop that iterates over each item in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 let pokemonList = [
-    { name: 'Pikachu', height: 0.4, type: ['electric'] },
-    { name: 'Squirtle', height: 0.5, type: ['water'] },
-    { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: ['electric'],
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: ['water'],
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire', 'flying'],
+    },
 ]
+/* A for loop:
+the initialization, or starting point (let i = 0)
+the condition (i <= 100)
+the action (i++)
+*/
+for (let i = 0; i < pokemonList.length; i++) {
+    /* pokemonList[i].name:
+    meaning give me the pokemon list name at position i, position i being 0, then 1, then 2 until the last object in the array
+    Template literal form aka Template Strings:
+    This is what a template literal form looks like: `hi ${message}` allows you to write anything between the backtick symbols.
+    */
+    document.write(
+        pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '<br>'
+    )
+}
