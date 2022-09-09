a6fc91e26c027afde503e0f590e2fe141a7b6e4d
rmoise <roderickfmoise@gmail.com>
2022-09-09 15:38:23 +0200

    refactor: :recycle: use a forEach() function instead of the for loop

--- js/scripts.js
+++ js/scripts.js
@@ -15,20 +15,11 @@ let pokemonList = [
         type: ['fire', 'flying'],
     },
 ]
-/* A for loop:
-the initialization, or starting point (let i = 0)
-the condition (i <= 100)
-the action (i++)
-*/
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.0) {
-        document.write(pokemonList[i].name + ` (height: ${pokemonList[i].height})` + " - Wow, that's big!")
+
+pokemonList.forEach(function (pokemon) {
+    if (pokemon.height > 1.0) {
+        document.write(pokemon.name + ` (height: ${pokemon.height})` + " - Wow, that's big!")
     } else {
-        /* pokemonList[i].name:
-        meaning give me the pokemon list name at position i, position i being 0, then 1, then 2 until the last object in the array
-        Template literal form aka Template Strings:
-        This is what a template literal form looks like: `hi ${message}` allows you to write anything between the backtick symbols.
-        */
-        document.write(pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '<br>')
+        document.write(pokemon.name + ` (height: ${pokemon.height})` + '<br>')
     }
-}
+})
