e7cd41fccc2dc44351ce5bc28281ad543e0689c2
rmoise <roderickfmoise@gmail.com>
2022-09-13 17:17:21 +0200

    refactor: :recycle: add IIFE to avoid accessing the global state

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,47 @@
-let pokemonList = [
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        type: ['electric'],
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        type: ['water'],
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        type: ['fire', 'flying'],
-    },
-]
+//@ts-check
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            type: ['electric']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            type: ['water']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            type: ['fire', 'flying']
+        }
+    ];
 
-pokemonList.forEach(function (pokemon) {
-    if (pokemon.height > 1.0) {
-        document.write(pokemon.name + ` (height: ${pokemon.height})` + " - Wow, that's big!")
-    } else {
-        document.write(pokemon.name + ` (height: ${pokemon.height})` + '<br>')
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && 'name' in pokemon) {
+            pokemonList.push(pokemon);
+        } else {
+            alert('Pokemon is incorrect');
+        }
     }
-})
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height > 1.0) {
+        document.write(pokemon.name + ` (height: ${pokemon.height})` + " - Wow, that's big!");
+    } else {
+        document.write(pokemon.name + ` (height: ${pokemon.height})` + '<br>');
+    }
+});
+
+console.log(pokemonRepository.getAll());
